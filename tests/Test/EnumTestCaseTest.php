87e25719715f1a9438c2af210b794105e4b31ce5
Oskar Stark <oskarstark@googlemail.com>
2022-10-20 10:46:35 +0200

    Enhancement: Add test for `EnumTestCase`

---
+++ tests/Test/EnumTestCaseTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of oskarstark/enum-helper.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OskarStark\Enum\Tests\Test;
+
+use OskarStark\Enum\Test\EnumTestCase;
+use OskarStark\Enum\Tests\Fixture\Color;
+use OskarStark\Enum\Tests\Fixture\NonBackedEnum;
+
+final class EnumTestCaseTest extends EnumTestCase
+{
+    protected static function getClass(): string
+    {
+        return Color::class;
+    }
+
+    protected static function getNumberOfValues(): int
+    {
+        return 2;
+    }
+}
